e1502223097e77077b751021d14641e632c9e46a
MangoStunt <50739312+MangoStunt@users.noreply.github.com>
2020-05-19 10:55:16 +0300

    Delete Lion.java

--- Animals/Lion.java
+++
@@ -1,9 +0,0 @@
-package Animals;
-
-public class Lion extends Mammal {
-
-    public Lion(String name, String classification, int age, int feedingPeriod) {
-        super(name, classification, age, feedingPeriod);
-    }
-
-}
