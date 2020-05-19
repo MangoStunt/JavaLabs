42f3509f4106206c4d16bd865d5516f4ca8a9579
MangoStunt <50739312+MangoStunt@users.noreply.github.com>
2020-05-19 10:55:02 +0300

    Delete Eagle.java

--- Animals/Eagle.java
+++
@@ -1,9 +0,0 @@
-package Animals;
-
-public class Eagle extends Bird {
-
-    public Eagle(String name, String classification, int age, int wingspan) {
-        super(name, classification, age, wingspan);
-    }
-
-}
